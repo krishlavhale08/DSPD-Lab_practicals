7a2bf5c4869e806cfb2b34398a04081591d8731a
krishlavhale08 <krishlavhale2006@gmail.com>
2025-11-16 23:25:21 +0530

    Add files via upload

---
+++ 10.c
@@ -0,0 +1,24 @@
+
+#include <stdio.h>
+int main(){
+    FILE *fs;
+    FILE *fd;
+    char ch;
+    fs = fopen("input.txt", "r");
+    fd = fopen("output.txt", "w");
+ 
+    
+    while(1) {
+        ch = fgetc(fs);
+        if (ch == EOF) {
+            break;
+        }
+        fputc(ch, fd);
+    }
+    
+    fclose(fs);
+    fclose(fd);
+    printf("Copied successfully!");
+    
+    return 0;
+}
